f4dc9022851b8c1cab78400076ce5a30db46e45c
Alessandro Rasia dal Polo <8293407@itisrossi.vi.it>
2024-01-16 18:01:01 +0100

    aggiunto esercizio HTTPRequest

--- WebServer-Configuration.Manager/src/HttpServer.java
+++ WebServer-Configuration.Manager/src/HttpServer.java
@@ -19,18 +19,40 @@ public class HttpServer {
             while(true){
                 Socket client = serverSocket.accept();
                 Thread thread = new Thread( () -> handleClient(client));
+                thread.start();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
     public static void handleClient(Socket socket){
+
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            System.out.println(br.read(new char[] {'c', 'i', 'a', 'o'}));
+            // Leggi la richiesta HTTP
+            StringBuilder request = new StringBuilder();
+            String line;
+            while (!(line = br.readLine()).isEmpty()) {
+                request.append(line).append("\r\n");
+            }
 
+            // Contenuto della pagina HTML di esempio
+            String htmlContent = "<html><head><title>Simple HTTP Server</title></head><body><h1>Hello, World!</h1></body></html>";
+
+            // Risposta HTTP con il contenuto della pagina HTML
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Length: " + htmlContent.length() + "\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "\r\n" +
+                    htmlContent;
+
+            // Invia la risposta al client
+            bw.write(response);
+            bw.flush();
+
+            // Chiudi flussi e socket
             br.close();
             bw.close();
             socket.close();
